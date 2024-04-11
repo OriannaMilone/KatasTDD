e7659490af6beae8835fe66ef08def1cd2d28f25
Orianna Milone <oriannamilone.s@gmail.com>
2024-04-11 22:46:54 +0200

    (FizzBuzz) Test 2

--- FizzBuzz/FizzingBuzzingTDD.py
+++ FizzBuzz/FizzingBuzzingTDD.py
@@ -12,6 +12,12 @@ class FizzBuzzKata(unittest.TestCase):
             result = self.numbers.smart_printer(i)
             self.assertEqual('Fizz', result)    
     
+    def test_multiple_of_five(self):
+        test_numbers = [5, 10, 15, 50]
+        for i in test_numbers: 
+            result = self.numbers.smart_printer(i)
+            self.assertEqual('Buzz', result) 
+               
     
     
 if __name__ == '__main__':
