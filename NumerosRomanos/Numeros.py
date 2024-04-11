f7ed9f81b7a876f069a38de90fe92fb6c1b71f00
Orianna Milone <oriannamilone.s@gmail.com>
2024-04-11 16:02:34 +0200

    Test 2 OK

--- NumerosRomanos/Numeros.py
+++ NumerosRomanos/Numeros.py
@@ -3,7 +3,7 @@
 class NumerosRomanos():
         
     def __init__(self):
-        self.numeros_base = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
+        self.numeros_base = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M', 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     
     def conversor_num_cardinales_romanos(self, numeros_cardinales):
         if not isinstance(numeros_cardinales, list):
@@ -12,3 +12,11 @@ class NumerosRomanos():
         for i in numeros_cardinales: 
             numeros_romanos.append(self.numeros_base[i])
         return numeros_romanos
+    
+    def conversor_num_romanos_cardinales(self, numeros_romanos):
+        if not isinstance(numeros_romanos, list):
+            numeros_romanos = list(numeros_romanos)
+        numeros_cardinales = []
+        for i in numeros_romanos: 
+            numeros_cardinales.append(self.numeros_base[i])
+        return numeros_cardinales
