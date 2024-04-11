9c6039d4be1a668c835bf40d8e46c61baa34f807
Orianna Milone <oriannamilone.s@gmail.com>
2024-04-11 16:06:59 +0200

    Test 3

--- NumerosRomanos/NRomanosTDD.py
+++ NumerosRomanos/NRomanosTDD.py
@@ -14,5 +14,10 @@ class NRomanosTDD(unittest.TestCase):
         numeros_transformados = self.num_romanos.conversor_num_romanos_cardinales(['I', 'V', 'X', 'L', 'C', 'D', 'M'])
         self.assertEqual([1,5,10,50,100,500,1000], numeros_transformados)
     
+    def test_numeros_pares(self):
+        numeros_transformados = self.num_romanos.conversor_num_cardinales_romanos([2,3,6,7,8])
+        self.assertEqual(['II', 'III', 'VI', 'VII', 'VIII'], numeros_transformados)
+        
+    
 if __name__ == '__main__':
     unittest.main()
